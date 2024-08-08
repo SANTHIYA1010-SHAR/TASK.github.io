cbe31ed1617f2cf772429074c002a63cfdbe04db
SANTHIYA1010-SHAR <santhiya10324@gmail.com>
2024-08-08 14:33:11 +0800

    Create App.vue

---
+++ src/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AssetTable />
+  </div>
+</template>
+
+<script>
+import AssetTable from './components/AssetTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AssetTable
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
